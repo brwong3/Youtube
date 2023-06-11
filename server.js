8277e71fd0aaf7bb2d999ff9e6261ec53e039073
Brandon <Brandon@Brandons-Laptop.hsd1.ca.comcast.net>
2023-06-11 01:16:44 -0700

    Got one thumbnail link to load

--- server.js
+++ server.js
@@ -13,19 +13,55 @@ app.get('/search/:searchTerm', async(req,res) => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
+
+
+
     await page.goto(`https://www.youtube.com/results?search_query=${req.params.searchTerm}`)
 
-    const scrapedData = await page.evaluate(() =>
-    Array.from(document.querySelectorAll('.ytd-video-renderer #video-title'))
-      .map(link => ({
-        title: link.getAttribute('title'),
-        link: link.getAttribute('href')
-      }))
-      .slice(0, 10)
-  )
+    /*TODO:
+        Get Title
+        Get Link
+        Get Artist
+        Get Thumbnail
+        Get Length of Video
+    */
+
+    // let scrapedArtists = await page.evaluate(() => 
+    //     Array.from(document.querySelectorAll("[id=dismissile] >.style-scope.ytd-channel-name > .yt-simple-endpoint.style-scope.yt-formatted-string"))
+    //         .map(link => {
+    //             return link.textContent;
+    //         })    
+    // )
+
+    // let scrapedData = await page.evaluate(() =>
+    //     Array.from(document.querySelectorAll('.ytd-video-renderer #video-title'))
+    //         .map(link => 
+    //         {
+    //             return {
+    //                 Title: link.getAttribute('title'),
+    //                 Link: `https://www.youtube.com${link.getAttribute('href')}`,
+    //             }
+    //         })
+    //         .slice(0, 30)
+    // ) 
+
+    await scrollPageToBottom(page);
+
+    let scrapedData = await page.evaluate(() => 
+        Array.from(document.querySelectorAll("div#dismissible"))
+            .map(element => {
+                return {
+                    Thumbnail: element.querySelector("ytd-thumbnail > a > yt-image > img").getAttribute("src")
+                }
+            })
+            .slice(0,20)
+    )
+    
 
     res.send(scrapedData)
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
