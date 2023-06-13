a945a8b8187cfe3301cf030463ea882453bcca2d
Brandon <Brandon@Brandons-Laptop.hsd1.ca.comcast.net>
2023-06-12 22:55:25 -0700

    Added Sections

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/search/:searchTerm/', async(req,res) => {
+app.get('/search/:searchTerm/:number', async(req,res) => {
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
@@ -24,24 +24,20 @@ app.get('/search/:searchTerm/', async(req,res) => {
         Get Length of Video
     */
 
-    await page.evaluate(() => 
-        scrollTo(0, 500)
-    )
-
     try {
-        let scrapedData = await page.evaluate(() => 
-        Array.from(document.querySelectorAll("div#dismissible.ytd-video-renderer"))
-            .map(element => {
-                return {
-                    Thumbnail: element.querySelector("ytd-thumbnail > a > yt-image > img").getAttribute("src"),
-                    Title: element.querySelector(".text-wrapper > #meta > #title-wrapper > h3 > a").getAttribute("title"),
-                    Artist: element.querySelector(".text-wrapper > #channel-info > ytd-channel-name > #container > #text-container > yt-formatted-string > a").textContent,
-                    Link: `https://youtube.com${element.querySelector("ytd-thumbnail > a").getAttribute("href")}`,
-                    Length: null
-                }
-            })
-        )
-        res.send(scrapedData);
+        let scrapedData = await page.evaluate(() => {
+            return Array.from(document.querySelectorAll("div#dismissible.ytd-video-renderer"))
+                .map(element => {
+                    return {
+                        Thumbnail: element.querySelector("ytd-thumbnail > a > yt-image > img").getAttribute("src"),
+                        Title: element.querySelector(".text-wrapper > #meta > #title-wrapper > h3 > a").getAttribute("title"),
+                        Artist: element.querySelector(".text-wrapper > #channel-info > ytd-channel-name > #container > #text-container > yt-formatted-string > a").textContent,
+                        Link: `https://youtube.com${element.querySelector("ytd-thumbnail > a").getAttribute("href")}`,
+                        Length: null
+                    }
+                })}
+            )
+            res.send(scrapedData.slice(0, req.params.number));
     } catch (error) {
         res.status(404);
     }
